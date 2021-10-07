19f6e587f33e8476dd417f0e3e949e833785c9e7
Pisagor <92087724+PisagorX@users.noreply.github.com>
2021-10-07 13:06:20 +0300

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -7,6 +7,15 @@ local isLoggedIn = false
 local PlayerData = {}
 local previousSkinData = {}
 
+ESX = nil
+
+Citizen.CreateThread(function()
+    while ESX == nil do
+        TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+        Citizen.Wait(0)
+    end
+end)
+
 local skinData = {
     ["face"] = {
         item = 0,
